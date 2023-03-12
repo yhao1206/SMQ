382bf42b335186950b7121031c3538133aab650b
yhao1206 <1161522231@qq.com>
2023-03-12 20:35:23 +0800

    delete useless code

--- message/messagge.go
+++ message/messagge.go
@@ -1,10 +1,5 @@
 package message
 
-import (
-	"bytes"
-	"encoding/gob"
-)
-
 type Message struct {
 	data []byte
 }
@@ -13,19 +8,6 @@ func NewMessage(data []byte) *Message {
 	return &Message{
 		data: data,
 	}
-}
-
-func NewMessageDecoded(data []byte) (msg *Message, err error) {
-	var (
-		b bytes.Buffer
-		d *gob.Decoder
-	)
-	if _, err = b.Write(data); err != nil {
-		return
-	}
-	d = gob.NewDecoder(&b)
-	err = d.Decode(msg)
-	return
 }
 
 func (m *Message) Uuid() []byte {
@@ -38,16 +20,4 @@ func (m *Message) Body() []byte {
 
 func (m *Message) Data() []byte {
 	return m.data
-}
-
-func (m *Message) Encode() ([]byte, error) {
-	var (
-		b   bytes.Buffer
-		e   = gob.NewEncoder(&b)
-		err error
-	)
-	if err = e.Encode(*m); err != nil {
-		return nil, err
-	}
-	return b.Bytes(), nil
 }
